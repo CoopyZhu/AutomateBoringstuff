b9cd8b7dc16162bebe05fc080f85e8f6435a1202
CoopyZhu <zcr19931224@gmail.com>
2018-10-26 20:17:54 +0800

    daliy

---
+++ DW/详情补充.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 19:57:54 2018
+
+@author: 朱诚锐
+"""
+
+from openpyxl import load_workbook
+
+
+file_dir = r"C:/Users/朱诚锐/OneDrive/DEEPWISE/问诊平台/数据/"
+file_name ="详情补充.xlsx"
+
+file_path = file_dir + file_name
+
+wkb = load_workbook(file_path)
+sheet = wkb.active
+
+for row in range(2,sheet.max_row+1):
+    content = sheet.cell(row=row,column=2).value
+    content = content.split("\n")
+    now_col = 3
+    con=""
+    for c in content:
+        if len(c)<10:
+            sheet.cell(row=row,column=now_col).value = con
+            con = ""
+            
+            now_col +=1
+            sheet.cell(row=row,column=now_col).value = c
+            now_col +=1
+        else:
+            con= con +c+"\n"
+    sheet.cell(row=row,column=now_col).value = con
+
+wkb.save("详情补充1.xlsx")
+
+
--- mingyan2/mingyan2/spiders/tags_spider.py
+++ mingyan2/mingyan2/spiders/tags_spider.py
@@ -17,27 +17,29 @@ class mingyantags(scrapy.Spider):
     count = 0
     
     def parse(self,response):
-        tags=response.xpath('//ul[@class="tags-list"]//a[@style]')
+        tags=response.css('ul.tags-list a[style]')
+        item = Mingyan2Item()
         for tag in tags:
-            Mingyan2Item.tag_urls=tag.xpath('@href').extract_first()
-            Mingyan2Item.tag_name=tag.xpath('text()').extract_first()
-            self.log(Mingyan2Item.tag_name)
-            mingyantags.ttags.append(Mingyan2Item.tag_name.strip())
-            yield scrapy.Request(Mingyan2Item.tag_urls,callback=self.parse_mingyan)
+            item["tag_urls"]=tag.css('::attr(href)').extract_first()
+            item["tag_name"]=tag.css('::text').extract_first().strip()
+            self.log(item["tag_name"])
+            yield scrapy.Request(item["tag_urls"],callback=self.parse_mingyan,meta=item)
             
             
     def parse_mingyan(self,response):
-        mingyan1 = response.xpath('//div[@class="quote post"]')
-        tag = response.xpath('//head//meta[@name="keywords"]/@content')[1].extract()
+        item=response.meta
+        self.log('item.keys{}'.format(item.keys))
+        mingyan1 = response.css('div[class="quote post"]')
+        tag = item["tag_name"]
         filename = tag+".txt"
         for m in mingyan1:
-            text = m.xpath('.//span[@class="text"]/text()').extract_first()
-            author = m.xpath('.//span/small/text()').extract_first()
+            text = m.css('.text::text').extract_first()
+            author = m.css('span small::text').extract_first()
             with open(filename,"a+") as f:
                 f.write("内容：{}\n作者：{}\n标签:{}\n".format(text,author,tag)) 
         self.log("保存文件：{}".format(filename)) #记录日志
         next_page= response.xpath('//li[@class="next"]//@href').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse_mingyan)
+            yield scrapy.Request(next_page, callback=self.parse_mingyan,meta=item)
         mingyantags.count+=1
